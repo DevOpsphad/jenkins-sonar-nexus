16ddd8b9a105ed695a0aab425d8974b96f1457d0
DevOpsphad <mohiniphad78@gmail.com>
2026-06-13 13:08:29 +0530

    instancetypeupdtae

--- jenkins-server.sh
+++ jenkins-server.sh
@@ -6,15 +6,17 @@
 ################################################################################################
 
 sudo apt update -y  # It will update repo 
-sudo apt install fontconfig openjdk-21-jre # It will install Openjdk
+sudo apt install -y fontconfig openjdk-21-jre-headless # It will install Openjdk
 
 sudo java -version  # It will check java version
 
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt update
 sudo apt install jenkins -y  # It will install jenkins
 echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
